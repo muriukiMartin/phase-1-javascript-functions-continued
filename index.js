f3ffb4768f8f53f6c7f3b7382319e16fa29dfb0e
Martin Muriuki <helheim254@gmail.com>
2023-06-14 20:42:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(task = 'roller-skate') {
+    return `This Saturday, I want to ${task}!`;
+}
+
+function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(sign) {
+    if (sign == '*' || sign =='||') {
+        return function wrap(adjective) {
+            return `You are ${sign}${adjective}${sign}!`;
+        }
+    }
+    
+}
